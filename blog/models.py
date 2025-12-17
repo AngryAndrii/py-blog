6a6a338ee32a909ad530a044f677d38b203ab66b
Andrii Chychkan <mywork1728@gmail>
2025-12-17 15:45:40 +0200

    added models

---
+++ blog/models.py
@@ -0,0 +1,41 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    def __str__(self):
+        return (f"{self.username} {self.first_name}"
+                f" {self.last_name} {self.email}")
+
+    class Meta:
+        ordering = ("username", )
+
+
+class Post(models.Model):
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL,
+                              related_name="posts",
+                              on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=False, null=False)
+    created_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.owner} / {self.created_time} - {self.content}"
+
+    class Meta:
+        ordering = ("title", )
+
+
+class Commentary(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             related_name="commentaries",
+                             on_delete=models.CASCADE)
+    post = models.ForeignKey(Post,
+                             related_name="commentaries",
+                             on_delete=models.CASCADE)
+    created_time = models.DateTimeField(auto_now_add=True)
+    content = models.TextField(blank=False, null=False)
+
+    def __str__(self):
+        return f"{self.user} / {self.created_time} - {self.content}"
